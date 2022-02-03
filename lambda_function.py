9f391554e0b7a2ab1705e13e33deaf55104c9e61
Elan Kainen <elank96@gmail.com>
2022-02-03 16:02:55 -0500

    added in fault tolerance and response mapping

--- lambda_function.py
+++ lambda_function.py
@@ -1,21 +1,31 @@
 import datetime
 
 def lambda_handler():
-    return {
-        "Status": 200,
-        "Body": calculateValidTaxYear() 
-        }
+    return responseMapper(calculateValidTaxYear())
 
 def calculateValidTaxYear():
-    taxDay = datetime.datetime(2022, 4, 18) #initialize Tax Day as datetime
-    currDate = datetime.datetime.now() #gets current date as datetime
-    taxDateToString = taxDay.strftime("%m/%d/%Y") #convert dateTime to string
-    taxYear = taxDateToString.split('/')[-1] #capture the year value of Tax Day as a String
+    try:
+        taxDay = datetime.datetime(2022, 4, 18) #initialize Tax Day as datetime
+        currDate = datetime.datetime.now() #gets current date as datetime
+        taxDateToString = taxDay.strftime("%m/%d/%Y") #convert dateTime to string
+        taxYear = taxDateToString.split('/')[-1] #capture the year value of Tax Day as a String
 
-    if currDate >= taxDay: # if the currentDate has passed or is Tax Day
-        return  {"Valid Tax Years" : [taxYear] } #return the current year we are in which will equal the current Tax year (this is a given)
-   
-    return {"Valid Tax Years" : [str(int(taxYear) - 1), taxYear]} #currentDate has not passed Tax Day, thus return last year and this year
+        if currDate >= taxDay: # if the currentDate has passed or is Tax Day
+            return  {0 : {"Valid Tax Years" : [taxYear] }} #return the current year we are in which will equal the current Tax year (this is a given)
+    
+        return {0: {"Valid Tax Years" : [str(int(taxYear) - 1), taxYear]}} #currentDate has not passed Tax Day, thus return last year and this year
+    
+    except Exception as ex:
+        return {1: ex}
 
-
-print(lambda_handler())
+def responseMapper(response):
+    if 0 in response:
+        return {
+            "Status": 200,
+            "Body": response[0]
+        }
+    if 1 in response:
+        return {
+            "Status": 500,
+            "Body": {"Internal Server Error": str(response[1])}
+        }
