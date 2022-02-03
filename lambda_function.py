44adf1de67a676f0a09fffe1ee755e033378ead3
Elan Kainen <elank96@gmail.com>
2022-02-03 15:45:52 -0500

    formatted response body with 200 code

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,10 @@
 import datetime
 
 def lambda_handler():
-    return calculateValidTaxYear()
+    return {
+        "Status": 200,
+        "Body": calculateValidTaxYear() 
+        }
 
 def calculateValidTaxYear():
     taxDay = datetime.datetime(2022, 4, 18) #initialize Tax Day as datetime
@@ -13,3 +16,6 @@ def calculateValidTaxYear():
         return  {"Valid Tax Years" : [taxYear] } #return the current year we are in which will equal the current Tax year (this is a given)
    
     return {"Valid Tax Years" : [str(int(taxYear) - 1), taxYear]} #currentDate has not passed Tax Day, thus return last year and this year
+
+
+print(lambda_handler())
